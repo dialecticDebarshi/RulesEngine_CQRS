6e85d05f47c37ba8db9691a9eae6f99b56712c3c
dialecticDebarshi <140203504+dialecticDebarshi@users.noreply.github.com>
2026-07-14 18:08:46 +0530

    Add files via upload

---
+++ RulesEngine.Contract/IHybridEvaluator.cs
@@ -0,0 +1,15 @@
+﻿using RuleEngine.Domain.Common;
+using RuleEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine.Contract
+{
+    public interface IHybridEvaluator
+    {
+        public  Task<RuleResponse> EvaluateAsync(HealthcareClaim claim);
+    }
+}
---
+++ RulesEngine.Contract/IWorkFlows.cs
@@ -0,0 +1,23 @@
+﻿using RuleEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine.Contract
+{
+        public interface IWorkFlows
+        {
+            public Task<IEnumerable<Workflow>> GetWorkflows();
+
+            public Task<Workflow> GetWorkflowById(Guid id);
+
+            public Task<Workflow> AddWorkflowAsync(Workflow workflow);
+            public Task<Workflow> EditWorkflowAsync(Workflow workflow, Guid id);
+            public Task<bool> RemoveWorkflowAsync(Guid id);
+            public Task<Workflow?> GetWorkflowByNameAsync(string workflowName);
+
+
+        }
+    }
